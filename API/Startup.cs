35f4cd1e655470ee5a1e660c022256d72745f83a
nickglas <40456410+nickglas@users.noreply.github.com>
2021-04-10 17:26:33 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,13 @@ namespace API
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
       });
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +61,8 @@ namespace API
       app.UseRouting();
 
       app.UseAuthorization();
+
+      app.UseCors("CorsPolicy");
 
       app.UseEndpoints(endpoints =>
       {
